057824ea9abdff17695d25b154a518b163103efe
Yohanes <yohanesdahlan@gmail.com>
2025-08-25 15:33:01 +0700

    implementing main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Product } from './pages/product';
+import { Pricing } from './pages/Pricing';
+import { Homepage } from './pages/Homepage';
+import { PageNotFound } from './pages/PageNotFound';
+// import Product from './pages/Product';
+
 export default function App() {
-  return <div>Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,3 @@
+export const PageNotFound = () => {
+  return <div>Page Not Found 404 :(</div>;
+};
