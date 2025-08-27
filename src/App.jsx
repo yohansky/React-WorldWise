81d59b977d26594df9fd73db252707d691f988f5
Yohanes <yohanesdahlan@gmail.com>
2025-08-27 21:38:53 +0700

    navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import CityList from './components/CityList';
 import axios from 'axios';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -53,7 +54,7 @@ export default function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
